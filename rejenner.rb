f1ae77de8dd432b404d9090fdc430e6ce20aba22
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2012-03-25 22:18:40 +1300

    initial commit

---
+++ rejenner.rb
@@ -0,0 +1,21 @@
+# A framework for static website generation which regenerates files in place.
+
+module Rejenner
+  
+  class Rejenner
+    def initialize(fileName)
+      @fileName = fileName
+    end
+    
+    def rejenerate
+      puts "Opening #{@fileName} ..."
+      File.open(@fileName).each_line do |line|
+        puts "line: #{line}"
+      end
+    end
+  end
+  
+  def self.rejenerate(fileName)
+    Rejenner.new(fileName).rejenerate
+  end
+end
