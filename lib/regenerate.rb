82eb581005cd6a0e0a3a9b00aaa09911ec03d184
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-06-12 10:07:14 +1200

    remove main-level code from lib/regenerate.rb (use script instead)

--- lib/regenerate.rb
+++ lib/regenerate.rb
@@ -3,12 +3,3 @@
 
 require 'regenerate/web-page.rb'
 require 'regenerate/site-regenerator.rb'
-
-STDOUT.sync = true
-STDERR.sync = true
-
-#puts "ARGV = #{ARGV.inspect}"
-
-if ARGV.length >= 1
-  Regenerate.regeneratePath(ARGV[0])
-end
